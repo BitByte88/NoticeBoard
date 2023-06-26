51047d29b4d4f82e6205eb01478898489ef49fb8
choi_jaehyoung <thisweek.tokyo@gmail.com>
2023-06-26 09:41:16 +0900

    不要な処理削除

--- src/main/java/jp/co/noticeBoard/service/BoardDetailService.java
+++ src/main/java/jp/co/noticeBoard/service/BoardDetailService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import jp.co.noticeBoard.dao.mapper.BoardDetailMapper;
@@ -18,10 +17,6 @@ import jp.co.noticeBoard.dto.BoardDetailDto;
 public class BoardDetailService {
 
     private static final Logger logger = LoggerFactory.getLogger(BoardDetailService.class);
-
-    //１ページあたりの最大件数
-    @Value("${list.max.count}")
-    private Integer limit;
 
     @Autowired
     private BoardDetailMapper boardDetailMapper;
